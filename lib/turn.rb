7bb4e711faae89f31c73625980c05805b679e417
Kaina Cockett <kainacockett@gmail.com>
2023-05-08 18:08:55 -0600

    Add first draft of Turn class, paritally tested

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,8 +4,18 @@ class Turn
         @guess = guess
         @card = card
     end
-    def correct
-        if @guess == @answer return true
+    def correct?
+        if @guess == @card.answer 
+            return true
+        else
+            return false
+        end
+    end
+    def feedback
+        if @guess == @card.answer
+            return "Correct!"
+        else
+            return "Incorrect"
         end
     end
 end
